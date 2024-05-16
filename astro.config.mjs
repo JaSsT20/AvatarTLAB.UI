23b8500cc92349d7d795d39c94e667a8462621f5
Levid <jassontejada@gmail.com>
2024-05-15 23:04:31 -0400

    Update Astro config and fix logo paths in Header and Layout components

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://avatar-api.github.io',
-  base: ''
+  base: '/avatar-api'
 });
